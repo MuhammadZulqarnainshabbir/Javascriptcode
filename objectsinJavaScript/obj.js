8a60231c93b9636239ff6a3dfc8474c6374fcc9d
ms18426 <muhammad.shabbir@stud.th-deg.de>
2022-05-23 04:15:03 +0200

    objects in java scripts

---
+++ objectsinJavaScript/obj.js
@@ -0,0 +1,12 @@
+const person = {
+    personName: "Ali",
+    personGender: "male",
+    personDOB: "18-09-2001",
+    personNationality: "German",
+    personhight: "155cm"
+}
+
+console.log("The name of person is:" + person.personName + "  " +
+    "The gender of person is:" + person.personGender
+
+);
