ded98baec5807cde8463e3dd725ad82e6277f55f
ms18426 <muhammad.shabbir@stud.th-deg.de>
2022-05-30 13:58:52 +0200

    secound Arrayjs

--- arraysinJavascript/arrayJs.js
+++ arraysinJavascript/arrayJs.js
@@ -1,8 +1,25 @@
 const nameArray = ["Ali", "Usman", "Ahmad"]; //Creating an array in javascript
 console.log(nameArray);
-nameArray[3] = Date.now();                   //Inserting current date at 4th index of Array
-console.log(nameArray)                       //printing the updated array in console
-let lengthArray = nameArray.length;         //geting length of Array
+
+
+//Inserting current date at 4th index of Array
+nameArray[3] = Date.now();
+console.log(nameArray)
+
+
+
+//geting length of Array
+let lengthArray = nameArray.length;
 console.log(lengthArray);
+
+//getting last element of array
+
 let lastEle = nameArray[lengthArray - 1];
 console.log(lastEle)
+
+//Looping the array
+for (var i = 0; i < nameArray.length; i++) {
+
+    console.log(nameArray[i]);
+}
+
