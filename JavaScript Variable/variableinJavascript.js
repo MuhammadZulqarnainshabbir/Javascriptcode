8a484e0310da024fda7e81b05ee39e5388bb290d
ms18426 <muhammad.shabbir@stud.th-deg.de>
2022-08-17 01:04:57 +0200

    Variables Javascript

---
+++ JavaScript Variable/variableinJavascript.js
@@ -0,0 +1,14 @@
+/*4 ways to declare a javascript varibale:
+using var
+using let
+using const
+using nothing*/
+
+//variables are containers for storing data
+
+//EXAMPLE
+
+var x = 5
+var y = 10
+var z = x + y
+console.log(z)
