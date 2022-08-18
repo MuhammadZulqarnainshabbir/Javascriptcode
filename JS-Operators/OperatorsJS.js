9e53b2880fa742745f30c9d30b83932dc521df5d
ms18426 <muhammad.shabbir@stud.th-deg.de>
2022-08-18 17:58:33 +0200

    OperatorsJS

---
+++ JS-Operators/OperatorsJS.js
@@ -0,0 +1,33 @@
+let x = 4;
+let y = 5;
+let z = x + y;
+console.log("Addition Answer" + " " + z)
+
+let r = x * y;
+console.log("Multiplication Answer" + " " + r)
+
+let sub = y - x;
+console.log("Subtraction " + sub)
+
+let Expo = x ** y;
+console.log(Expo);
+
+let Div = x / y;
+console.log("Divison " + Div)
+
+
+let MOD = x % y;
+console.log(MOD);
+
+let inc = ++x
+console.log("increment" + inc)
+
+let dec = --x
+console.log("Decrement" + dec);
+
+console.log(5 & 1)
+
+console.log(typeof (x))
+
+let exp = (100 + 50) * 3
+console.log(exp)
