74d7775758832774f262391bf7bbfe21350f23d2
ms18426 <muhammad.shabbir@stud.th-deg.de>
2022-09-02 04:02:46 +0200

    JS-TypeConversion

---
+++ JS-Typeconversion/JsTypeconversion.js
@@ -0,0 +1,18 @@
+let b = Number("3.14")
+console.log(typeof (b))
+
+let a = 123
+let c = String(a)
+console.log(typeof (c))
+
+let d = 201
+console.log(typeof (d.toString()))
+
+d = new Date()
+console.log(d.getTime())
+
+
+console.log(String(d))
+
+console.log(Number(false))
+
